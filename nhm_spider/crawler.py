429ab592d58eddce1f6c424b115ea759dd314b4e
nhm <zongxuheng@163.com>
2022-02-23 18:54:54 +0800

    develop: 1.34 使用更高效的循环方式，在task外部执行获取任务步骤，task内部只处理当前请求的任务

--- nhm_spider/crawler.py
+++ nhm_spider/crawler.py
@@ -8,22 +8,38 @@
     @Project : nhm-spider
 """
 import asyncio
-from inspect import isawaitable
+from asyncio import Semaphore, Future, wait
+from inspect import isawaitable, iscoroutine
+from traceback import format_exc
+from types import GeneratorType, AsyncGeneratorType
 
+from nhm_spider import Request, Response, Item
+from nhm_spider.common.log import get_logger
 from nhm_spider.common.time_counter import time_limit
 from nhm_spider.core.downloader import Downloader
 from nhm_spider.core.scheduler import Scheduler
 from nhm_spider.exceptions import NoCrawlerError
+from nhm_spider.utils.request import request_fingerprint
 from nhm_spider.utils.signal import SignalManager
 
 
 class Crawler:
     def __init__(self, spider_class):
+        self.logger = get_logger("Crawler")
         self.spider = spider_class.from_crawler(crawler=self)
         self.downloader = Downloader(self.spider)
         self.scheduler = Scheduler(self.spider)
 
         self.concurrent_requests: int = self.spider.settings.get_int("CONCURRENT_REQUESTS", 8)
+        # pipeline
+        enabled_pipeline = self.spider.settings.get_list("ENABLED_PIPELINE")
+        self.enabled_pipeline = [cls() for cls in enabled_pipeline]
+        # download middleware
+        enabled_download_middleware = self.spider.settings.get_list("ENABLED_DOWNLOAD_MIDDLEWARE")
+        self.enabled_download_middleware = [cls() for cls in enabled_download_middleware]
+        # spider middleware
+        # enabled_spider_middleware = settings.get_list("ENABLED_SPIDER_MIDDLEWARE")
+        # self.enabled_spider_middleware = [cls() for cls in enabled_spider_middleware]
 
     @time_limit(display=True)
     def run(self):
@@ -41,7 +57,7 @@ class Crawler:
         self.signal_manager.connect()
 
         # init pipeline
-        for pipeline in self.scheduler.enabled_pipeline:
+        for pipeline in self.enabled_pipeline:
             # 确认是否使用的异步的pipeline
             if not hasattr(pipeline, "open_spider"):
                 continue
@@ -50,7 +66,7 @@ class Crawler:
                 await pip
 
         # init download middleware
-        for middleware in self.scheduler.enabled_download_middleware:
+        for middleware in self.enabled_download_middleware:
             # 确认是否使用的异步的middleware
             if not hasattr(middleware, "open_spider"):
                 continue
@@ -63,7 +79,7 @@ class Crawler:
         退出crawler的准备操作
         """
         # clear pipeline
-        for pipeline in self.scheduler.enabled_pipeline:
+        for pipeline in self.enabled_pipeline:
             # 确认是否使用的异步的pipeline
             if not hasattr(pipeline, "close_spider"):
                 continue
@@ -72,7 +88,7 @@ class Crawler:
                 await pip
 
         # clear download middleware
-        for middleware in self.scheduler.enabled_download_middleware:
+        for middleware in self.enabled_download_middleware:
             # 确认是否使用的异步的middleware
             if not hasattr(middleware, "close_spider"):
                 continue
@@ -84,28 +100,46 @@ class Crawler:
         """
         协程主程序
         """
+        def callback(future: Future):
+            semaphore.release()
+            exception = future.exception()
+            if exception:
+                raise exception
+
         # todo: 应打印初始化了哪些模块。
 
         await self._open_crawler()
 
         tasks = []
+        semaphore = Semaphore(value=self.concurrent_requests)
         try:
             # 初始化
             results = self.spider.start_request()
-            await self.scheduler.process_results(results)
-            for _ in range(self.concurrent_requests):
-                task = asyncio.create_task(self.scheduler.process(self.downloader))
-                tasks.append(task)
-
-            # 阻塞并等待所有任务完成   todo: heartbeat应放置到单独模块中去
+            await self.process_results(results)
+            # todo: heartbeat应放置到单独模块中去
             tasks.append(asyncio.create_task(self.scheduler.heartbeat()))
+            while 1:
+                # 强制退出时候退出循环 todo: 续判断退出到时候当前已经开始的任务是否已经执行完？
+                if self.scheduler.request_queue._finished.is_set():
+                    break
+                # 所有任务都已经处理完时，执行退出循环
+                if self.scheduler.request_queue.empty() and semaphore._value == self.concurrent_requests:
+                    break
+                rs = await wait([self.scheduler.next_request()], timeout=1)
+                if not rs[0]:
+                    continue
+                request = rs[0].pop().result()
+                await semaphore.acquire()
+                # todo
+                asyncio.create_task(self.process(request)).add_done_callback(callback)
+
+            # 阻塞并等待所有任务完成
             await self.scheduler.request_queue.join()
 
             # 正常推出时执行的关闭
             success_close_task = self.spider.custom_success_close()
             if isawaitable(success_close_task):
                 await success_close_task
-
         finally:
             await self._close_crawler()
 
@@ -125,6 +159,149 @@ class Crawler:
             tasks.clear()
 
             # todo: 应打印采集完成汇总的数据。
+
+    async def process_results(self, results, response=None):
+        if results:
+            if isinstance(results, GeneratorType):
+                for obj in results:
+                    await self.process_result_single(obj, response)
+            elif isinstance(results, AsyncGeneratorType):
+                async for obj in results:
+                    await self.process_result_single(obj, response)
+            elif isinstance(results, Request):
+                await self.process_result_single(results, response)
+            elif iscoroutine(results):
+                await results
+            else:
+                # todo: 考虑如何处理
+                self.logger.error(f"丢弃该任务，未处理的处理结果类型：{results}。")
+
+    async def process_result_single(self, obj, response):
+        if isinstance(obj, Request):
+            # 处理request对象优先级，深度优先
+            if obj.priority is None:
+                if response is not None:
+                    obj.priority = response.request.priority - 1
+                else:
+                    obj.priority = 0
+
+            fp = request_fingerprint(obj)
+            # 根据指纹去重。
+            if obj.dont_filter is True or fp not in self.scheduler.dupe_memory_queue:
+                obj.fp = fp
+                await self.scheduler.enqueue_request(obj)
+                self.scheduler.dupe_memory_queue.add(fp)
+
+        elif isinstance(obj, Item):
+            if not self.enabled_pipeline and self.spider.DEBUG is True:
+                self.logger.info(obj)
+            self.scheduler.item_count += 1
+
+            for pipeline in self.enabled_pipeline:
+                # 确认是否使用的异步的pipeline
+                if not hasattr(pipeline, "process_item"):
+                    continue
+                obj = pipeline.process_item(obj, self.spider)
+                if isawaitable(obj):
+                    obj = await obj
+
+        else:
+            self.logger.warning(f"[yield]尚未处理的类型[{obj.__class__.__name__}]。")
+
+    async def process(self, request):
+
+        response = await self.download_request(request)
+        if not isinstance(response, Response):
+            # todo: 待处理非response的情况
+
+            # 失败的请求也要调用task_done，否则无法结束。
+            self.scheduler.request_queue.task_done()
+            self.scheduler.request_count += 1
+            return
+        else:
+            if self.spider.DEBUG is True:
+                self.logger.info(f"Crawled ({response.status}) {response}.")
+
+        # todo: process_spider_in
+        results = request.callback(response)
+        # todo: process_spider_out 非此位置
+        try:
+            await self.process_results(results, response)
+        except:
+            self.logger.error(format_exc())
+        finally:
+            self.scheduler.request_queue.task_done()
+            self.scheduler.request_count += 1
+
+    async def download_request(self, request):
+        # process_request
+        for middleware in self.enabled_download_middleware:
+            # 确认是否使用的异步的middleware
+            if not hasattr(middleware, "process_request"):
+                continue
+            result = middleware.process_request(request, self.spider)
+            if isawaitable(result):
+                result = await result
+
+            if result is None:
+                pass
+            elif isinstance(result, Request):
+                return await self.process_results(result)
+            elif isinstance(result, Response):
+                # 返回response则直接跳过process_request
+                request = result
+                break
+            else:
+                self.logger.error(f"未知的对象类型，{request}。")
+                raise TypeError("未知的对象类型")
+
+        if isinstance(request, Request):
+            response = await self.downloader.send_request(request)
+        elif isinstance(request, Response):
+            response = request
+        else:
+            self.logger.error(f"未知的对象类型，{request}。")
+            raise TypeError("未知的对象类型")
+
+        # process_response
+        if isinstance(response, Response):
+            for middleware in self.enabled_download_middleware:
+                # 确认是否使用的异步的middleware
+                if not hasattr(middleware, "process_response"):
+                    continue
+                result = middleware.process_response(request, response, self.spider)
+                if isawaitable(result):
+                    result = await result
+
+                if result is None:
+                    pass
+                elif isinstance(result, Request):
+                    return await self.process_results(result)
+                elif isinstance(result, Response):
+                    response = result
+                    break
+        elif isinstance(response, Exception):
+            for middleware in self.enabled_download_middleware:
+                # 确认是否使用的异步的middleware
+                if not hasattr(middleware, "process_exception"):
+                    continue
+                result = middleware.process_exception(request, response, self.spider)
+                if isawaitable(result):
+                    result = await result
+
+                if result is None:
+                    pass
+                elif isinstance(result, Request):
+                    return await self.process_results(result)
+                elif isinstance(result, Response):
+                    response = result
+                    break
+
+        else:
+            self.logger.error(f"未知的Response类型，{response}。")
+            raise TypeError("未知的Response类型")
+
+        return response
 
 
 class CrawlerRunner:
